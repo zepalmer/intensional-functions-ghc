ghc/ghc

Make UnfoldingGuidance account for free variables
For over a decade (#3781) I've wondered whether GHC's
UnfoldingGuidance should take account of free variables.  For example:

   f x = let g y = case x of ....
         in
         ...(case x of ....g e....)...

If we inline the call to g in the body of the case x, we can save
re-evaluating (and case analysis of) x. This very similar to the
"discounts" we give for arguments.  All we need is to do it for
free variables.

It turned out to be rather easy.

* Beef up GHC.Core.Unfold.sizeExpr to accumulate discounts for
  free variables.

* Beef up GHC.Core.Unfold.callSiteInline to compute those
  discounts, based on info in the InScopeSet

Fixes #3781
Also fixes #19643 (which reminded me of this issue)

compiler/GHC/Core/Opt/Simplify.hs
@@ -1947,7 +1947,7 @@ simplIdF env var cont
 
 completeCall :: SimplEnv -> OutId -> SimplCont -> SimplM (SimplFloats, OutExpr)
 completeCall env var cont
-  | Just expr <- callSiteInline logger dflags case_depth var active_unf
+  | Just expr <- callSiteInline logger dflags in_scope case_depth var active_unf
                                 lone_variable arg_infos interesting_cont
   -- Inline the variable's RHS
   = do { checkedTick (UnfoldingDone var)
@@ -1966,6 +1966,7 @@ completeCall env var cont
   where
     dflags     = seDynFlags env
     case_depth = seCaseDepth env
+    in_scope   = seInScope env
     logger     = seLogger env
     (lone_variable, arg_infos, call_cont) = contArgs cont
     n_val_args       = length arg_infos

zepalmer/intensional-functions-ghc

compiler/GHC/Core/Opt/Simplify.hs
@@ -2008,7 +2008,7 @@ simplIdF env var cont
 
 completeCall :: SimplEnv -> OutId -> SimplCont -> SimplM (SimplFloats, OutExpr)
 completeCall env var cont
-  | Just expr <- callSiteInline logger uf_opts case_depth var active_unf
+  | Just expr <- callSiteInline logger uf_opts in_scope case_depth var active_unf
                                 lone_variable arg_infos interesting_cont
   -- Inline the variable's RHS
   = do { checkedTick (UnfoldingDone var)
@@ -2027,6 +2027,7 @@ completeCall env var cont
   where
     uf_opts    = seUnfoldingOpts env
     case_depth = seCaseDepth env
+    in_scope   = seInScope env
     logger     = seLogger env
     (lone_variable, arg_infos, call_cont) = contArgs cont
     n_val_args       = length arg_infos